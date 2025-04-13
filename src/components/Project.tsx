f8c19689520b98f3c0f453e427bb4d114eba32cc
Marquinhos <fernandez9029@gmail.com>
2025-04-13 16:44:13 -0500

    ok, pagina principal

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -25,12 +25,17 @@ const Project = () => {
         Outstanding Projects
       </h1>
       {/* cards */}
-      <div className={`flex justify-center items-center gap-5 w-full pt-5`}>
+      <div
+        className={`flex justify-center items-center gap-1 md:gap-5 w-full pt-5`}
+      >
         {cards.map((_, i) => (
+          // Agregar psudelemento a este div con un capa ligeramente oscura, ojo de que puede afectar los z-index, el orden de los elementos debe ser: este div, encima el psudoelemento, luego la imagen, luego el texto (p y span)
           <div
             key={i}
             className={`h-72 rounded-4xl bg-amber-500 transition-all duration-500 shadow relative overflow-hidden flex justify-start items-end p-3 border-4 border-amber-500 ${
-              activeIndex === i ? "w-[28rem]" : "w-16"
+              activeIndex === i
+                ? "w-[28rem] card-active-with-overlay"
+                : "w-16 card-with-overlay"
             }`}
             onClick={() => setActiveIndex(i)}
           >
@@ -54,9 +59,36 @@ const Project = () => {
 
             {/* Inicial con z-10 para estar por encima */}
             <div
-              className={`h-10 w-10 bg-amber-500 text-white flex items-center justify-center rounded-full z-10 relative font-black text-2xl`}
+              className={`flex ${
+                activeIndex === i ? "flex-row-reverse items-end" : "flex-col"
+              } justify-center gap-2 transition-all duration-500 relative z-20`}
             >
-              {i === 0 ? "K" : i === 1 ? "J" : i === 2 ? "L" : "T"}
+              <div
+                className={`text-white font-bold transition-all duration-500 origin-left ${
+                  activeIndex === i ? "rotate-0 " : "-rotate-90 translate-x-2"
+                }`}
+              >
+                {i === 0
+                  ? "Kassya"
+                  : i === 1
+                  ? "Jana"
+                  : i === 2
+                  ? "Leangineer"
+                  : "Tasky"}
+                <button
+                  className={`bg-white hover:bg-indigo-700 hover:text-white transition-all duration-500 text-black px-2 py-1 rounded-xl font-normal mt-2 ${
+                    activeIndex === i ? "block" : "hidden"
+                  }`}
+                >
+                  + More info
+                </button>
+              </div>
+
+              <span
+                className={`h-6 w-6 lg:h-8 lg:w-8 bg-amber-500 text-white flex items-center justify-center rounded-full z-10 font-black md:text-lg lg:text-xl`}
+              >
+                {i === 0 ? "K" : i === 1 ? "J" : i === 2 ? "L" : "T"}
+              </span>
             </div>
           </div>
         ))}
